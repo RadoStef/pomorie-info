10b8207abaae4fc2584f8f83aae4833131b2cb79
RadoStef <rs_perfect@mail.bg>
2022-06-16 22:24:20 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+
+      600: {
+        items: 2
+      },
+
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+
+  
+});
