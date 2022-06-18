dd541dbda0e55498dfee4aabcd0d3213f3be877c
RadoStef <rs_perfect@mail.bg>
2022-06-18 21:21:13 +0300

    City objects added

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ $(document).ready(function(){
   const logoEl = document.getElementById("logo-el");
   const footerEl = document.getElementById("footer-el");
   const sourcesEl = document.getElementById("sources-el");
+  const navigationEl = document.getElementById("navigation-el");
 
   let sources = [`<a href="https://www.wikipedia.org">Wikipedia</a>,  
                   <a href="https://pomorietourism.bg/"> Pomorietourism</a>, 
@@ -37,9 +38,78 @@ $(document).ready(function(){
                 ];
 
 
-  logoEl.innerHTML = `<img src="logo.png" alt="site_logo">`
+  logoEl.innerHTML = `<img src="https://www.sitefocus.eu/logo.png" alt="site_logo">`
   footerEl.textContent = "pomorie-info.com 2022. v1.0.22"
   sourcesEl.innerHTML = sources;
+
+  navigationEl.innerHTML = `<ul>
+  <li>
+      <h4>Забележителности и история</h4>
+      <a href="#">Музеи</a>
+      <a href="#">Забележителности</a>
+      <a href="#">История на Поморие</a>
+  </li>
+
+  <li>
+      <h4>Спорт и рекреация</h4>
+      <a href="#">Спортни обекти</a>
+      <a href="#">Разходка, джогинг, вело</a>
+      <a href="#">Санаториуми</a>
+      <a href="#">Луга и калолечение</a>
+  </li>
+
+  <li>
+      <h4>За децата</h4>
+      <a href="#">Детски площадки</a>
+      <a href="#">Заведения с детски кът</a>
+  </li>
+
+  <li>
+      <h4>Плажове</h4>
+      <a href="#">Южен плаж - нов град</a>
+      <a href="#">Северен плаж - Крайбрежна</a>
+      <a href="#">Централен плаж - Атриум</a>
+      <a href="#">Местност "Косата"</a>
+  </li>
+
+
+  <li>
+      <h4>Паркинги</h4>
+      <a href="#">Платени и безплатни паркинги</a>
+  </li>
+
+  <li>
+      <h4>Атракциони</h4>
+      <a href="#">Морска разходка</a>
+      <a href="#">Влакче</a>
+      <a href="#">Плажни атракциони</a>
+      <a href="#">Лунапарк</a>
+      <a href="#">Аквапарк</a>
+  </li>
+
+  <li>
+      <h4>Транспорт</h4>
+      <a href="#">Градски автобуси</a>
+      <a href="#">Таксита</a>
+  </li>
+
+  <li>
+      <h4>Култура</h4>
+      <a href="#">Читалища</a>
+      <a href="#">Книжарници</a>
+  </li>
+
+  <li>
+      <h4>Хранене</h4>
+      <a href="#">Ресторанти</a>
+      <a href="#">Бързо хранене</a>
+      <a href="#">Сладкарници и кафенета</a>
+      <a href="#">Нощни заведения</a>
+      <a href="#">Бийч барове</a>
+  </li>
+
+  <li><button class="menu-close-btn"><i class="fa-solid fa-xmark"></i></button></li>
+</ul>`
   
 
 });
